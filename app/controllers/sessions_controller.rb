f760391d726db06b767f406967a9d8f5d3a9e82c
Eric Fiorani <efiorani305@gmail.com>
2018-12-20 16:24:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   before_action :require_login
-  
+
   def new
   end
 
   def create
     if params[:name].nil? || params[:name].empty?
-      redirect_to '/login'
+      redirect_to '/sessions/new'
     else
       session[:name] = params[:name]
       redirect_to '/'
