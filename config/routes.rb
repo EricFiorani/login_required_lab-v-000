43046c4157ff67bb4ac868aec1cf2984e2552352
Eric Fiorani <efiorani305@gmail.com>
2018-12-20 16:30:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'application#hello'
   get '/login' => 'sessions#new'
-  
+  get '/big_secret' => 'secrets#show'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
